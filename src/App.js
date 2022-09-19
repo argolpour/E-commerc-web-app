b387f9bf3eda636fbb42941ddd8a9491c0ab55c0
argolpour <argolpour@gmail.com>
2022-09-19 13:43:01 +0430

    sdding navigation bar + styles and put logo on  it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Home from "./Routes/home/Home";
 import { Routes, Route } from 'react-router-dom'
+import Navigation from "./Routes/Navigation/Navigation";
 
-
+const Shop = () => {
+  return (
+    <h2>Shop Container</h2>
+  )
+}
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
---
+++ src/Routes/Navigation/Navigation.jsx
@@ -0,0 +1,22 @@
+import { Link, Outlet } from "react-router-dom"
+import './navigation.styles.scss'
+import { ReactComponent as Crown } from "../../assests/svg/crown.svg"
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to='/'>
+          <Crown />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link " to='/shop'>
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  )
+}
+
+export default Navigation
