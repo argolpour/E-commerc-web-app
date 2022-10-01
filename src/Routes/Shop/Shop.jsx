ba89d088598afcf4b6dba0dc89c4f4e2ef25f2b0
argolpour <argolpour@gmail.com>
2022-10-01 15:55:35 +0330

    product card container added

--- src/Routes/Shop/Shop.jsx
+++ src/Routes/Shop/Shop.jsx
@@ -1,12 +1,12 @@
+import './shop.styles.scss'
 import { useContext } from 'react'
+import ProductCard from '../../components/product-card/ProductCard'
 import { ProductsContext } from '../../context/productsContext'
 const Shop = () => {
   const { products } = useContext(ProductsContext)
   return (
-    <div>
-      {products.map(({ id, name }) => <div key={id}>
-        <h1>{name}</h1>
-      </div>)}
+    <div className='products-container container'>
+      {products.map(product => <ProductCard key={product.id} product={product} />)}
     </div>
   )
 }
