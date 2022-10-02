7fc14002dc56e70803250c3a8ff0d66e6945bb18
argolpour <argolpour@gmail.com>
2022-10-02 13:30:01 +0330

    increment and decrement in cart checkout added

--- src/components/checkout-item/CheckoutItem.jsx
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -3,9 +3,15 @@ import { CartContext } from '../../context/cartContext';
 import './checkout-item.styles.scss'
 const CheckoutItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
-  const { clearItemFromCart } = useContext(CartContext)
+  const { clearItemFromCart, addItemToCart, removeItemFromCart } = useContext(CartContext)
   const clearItemFromCartHandler = () => {
     clearItemFromCart(cartItem)
+  }
+  const incrementHndler = () => {
+    addItemToCart(cartItem)
+  }
+  const decrementHndler = () => {
+    removeItemFromCart(cartItem)
   }
   return (
     <div className="checkout-item-container">
@@ -13,7 +19,11 @@ const CheckoutItem = ({ cartItem }) => {
         <img src={imageUrl} alt={`${name}`} />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className='arrow' onClick={decrementHndler}>&#10094;</div>
+        <span className='value'>{quantity}</span>
+        <div className='arrow' onClick={incrementHndler}>&#10095;</div>
+      </span>
       <span className="price">{price}</span>
       <span className="remove-button" onClick={clearItemFromCartHandler}>&#10005;</span>
     </div>
