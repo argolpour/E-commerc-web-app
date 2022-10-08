e951021382c2e4521fcd8aa3b286fc59790ecca2
argolpour <argolpour@gmail.com>
2022-10-08 12:35:07 +0330

    category preview  components imigrated to styled components

--- src/components/category-preview/CategoryPreview.jsx
+++ src/components/category-preview/CategoryPreview.jsx
@@ -1,19 +1,19 @@
-import './category-preview.styles.scss'
+import { CategoryPreviewContainer, Title, Preview } from './category-preview.styles.js'
 import ProductCard from "../product-card/ProductCard"
 import { Link } from 'react-router-dom'
 
 const CategoryPreview = ({ title, products }) => {
   return (
-    <div className="category-preview-container">
-      <h2>
+    <CategoryPreviewContainer className="category-preview-container">
+      <Title>
         <Link to={title}> <span className="title">{title.toUpperCase()}</span></Link>
-      </h2>
-      <div className="preview">
+      </Title>
+      <Preview className="preview">
         {
           products.filter((_, index) => index < 4).map((product) => <ProductCard key={product.id} product={product} />)
         }
-      </div>
-    </div>
+      </Preview>
+    </CategoryPreviewContainer>
   )
 }
 
