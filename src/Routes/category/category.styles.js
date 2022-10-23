ed2ba75552ed815cc72e08855b68e710fecf4643
argolpour <argolpour@gmail.com>
2022-10-23 12:16:05 +0330

    responsive styles added

--- src/Routes/category/category.styles.js
+++ src/Routes/category/category.styles.js
@@ -6,22 +6,19 @@ export const CategoryContainer = styled.div`
   column-gap: 20px;
   row-gap: 50px;
     @media (max-width:992px)  {
- .category-container {
   display: grid;
   grid-template-columns: repeat(2,1fr);
   column-gap: 20px;
   row-gap: 50px;
   
- }
+ 
 }
 @media (max-width:768px)  {
-.category-container {
   display: grid;
   grid-template-columns: repeat(1,1fr);
+    row-gap: 30px;
   
-  row-gap: 30px;
-  
- }
+ 
 }
 `
 export const CategoryTitle = styled.div`
--- src/components/category-preview/category-preview.styles.js
+++ src/components/category-preview/category-preview.styles.js
@@ -4,7 +4,7 @@ export const CategoryPreviewContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 30px;
- 
+
 `
 export const Title = styled.h2`
     font-size: 28px;
@@ -16,4 +16,12 @@ export const Preview = styled.div`
    display: grid;
     grid-template-columns: repeat(4, 1fr);
     column-gap: 20px;
+       @media (max-width:992px)  {
+   grid-template-columns: repeat(2, 1fr);
+    column-gap: 20px;
+       }
+         @media (max-width:576px)  {
+   grid-template-columns: repeat(1, 1fr);
+    column-gap: 20px;
+       }
   `
